15d20fbf13e66eb1934fdc5dddfcf4d464941d20
ArtemUstinov <arti-0@mail.ru>
2020-10-24 17:10:28 +0300

    Initial Commit

---
+++ SweetyPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweetyPhoto
+//
+//  Created by Артём Устинов on 24.10.2020.
+//  Copyright © 2020 Artem Ustinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
